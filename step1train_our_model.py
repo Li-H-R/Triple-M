5d51e25a1c7e42ab95a7636d3c3746e6633dc7da
Li-H-R <wang21116011@gmail.com>
2024-12-12 20:49:01 +0800

    “本次上传的提示,随便你写什么都可以”，

---
+++ step1train_our_model.py
@@ -0,0 +1,240 @@
+import torch
+import pickle
+import torch.nn as nn
+import math
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import torch.nn.functional as F
+
+
+def data_used(per):
+    save_path = f'train_dataset/KIL_DataSet_{per}%.pt'
+    data_set = torch.load(save_path)
+
+    return data_set
+
+
+# 位置编码
+class PositionalEncoding(nn.Module):
+    "Implement the PE function."
+
+    def __init__(self, d_model, dropout, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        # 初始化Shape为(max_len, d_model)的PE (positional encoding)
+        pe = torch.zeros(max_len, d_model)
+        # 初始化一个tensor [[0, 1, 2, 3, ...]]
+        position = torch.arange(0, max_len).unsqueeze(1)
+        # 这里就是sin和cos括号中的内容，通过e和ln进行了变换
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model)
+        )
+        # 计算PE(pos, 2i)
+        pe[:, 0::2] = torch.sin(position * div_term)
+        # 计算PE(pos, 2i+1)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        # 为了方便计算，在最外面在unsqueeze出一个batch
+        pe = pe.unsqueeze(0)
+        # 如果一个参数不参与梯度下降，但又希望保存model的时候将其保存下来
+        # 这个时候就可以用register_buffer
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        """
+        x 为embedding后的inputs，例如(1,7, 128)，batch size为1,7个单词，单词维度为128
+        """
+        # 将x和positional encoding相加。
+        x = x + self.pe[:, : x.size(1)].requires_grad_(False)
+        return self.dropout(x)
+
+
+class STtrans_C_FNN(nn.Module):
+
+    def __init__(self, input_dim=13, d_model=16, window_l=11, encoder_l=128):
+        super(STtrans_C_FNN, self).__init__()
+
+        # 原始数据维度为13，扩展编码为16 便于多投注意力机制
+        self.embedding_encoder_T = nn.Linear(input_dim, d_model)
+        self.embedding_encoder_S = nn.Linear(window_l, d_model)
+        # 定义位置编码器
+        self.positional_encoding = PositionalEncoding(d_model, dropout=0)
+        self.encoder_time = nn.TransformerEncoder(
+            encoder_layer=nn.TransformerEncoderLayer(
+                d_model=d_model,  # 输入数据的特征维度
+                nhead=8,  # 注意力头数
+                dim_feedforward=1024,
+                dropout=0.1,
+                batch_first=True
+            ),
+            num_layers=1,
+        )
+        self.encoder_space = nn.TransformerEncoder(
+            encoder_layer=nn.TransformerEncoderLayer(
+                d_model=d_model,  # 输入数据的特征维度
+                nhead=8,  # 注意力头数
+                dim_feedforward=1024,
+                dropout=0.1,
+                batch_first=True
+            ),
+            num_layers=1,
+        )
+
+        self.space_linear = nn.Linear(input_dim, d_model)
+        self.layer_fusion = nn.Linear((input_dim + window_l) * d_model, encoder_l)
+
+        # 解码器
+        self.layers_decoder = nn.Sequential(
+            nn.Linear(encoder_l, 160),
+            nn.GELU(),
+            nn.Linear(160, 160),
+            nn.GELU(),
+            nn.Linear(160, input_dim * window_l)
+        )
+        # 仿射层
+        self.re_linear = nn.Linear(6*7, encoder_l)
+
+    def forward(self, src, k_i=None):
+        # 对src进行编码
+        src_time = self.embedding_encoder_T(src)
+        src_space = src.permute(0, 2, 1)
+        src_space = self.embedding_encoder_S(src_space)
+
+        # 给src和tgt的token增加位置信息
+        src_time = self.positional_encoding(src_time)
+
+        # 将准备好的数据送给transformer
+        memory_out_time = self.encoder_time(src_time)
+        # 不用转置，使得存储连续，方便训练
+        memory_out_space = self.encoder_space(src_space)
+
+        memory_out = torch.cat((memory_out_time, memory_out_space), dim=1)
+        reshape_x = memory_out.view(-1, memory_out.shape[1] * memory_out.shape[2])
+        memory_out = self.layer_fusion(reshape_x)
+        x_re = self.layers_decoder(memory_out)
+        if k_i is not None:
+            text_out = self.re_linear(k_i)
+        else:
+            text_out = None  # or provide a default value if needed
+        return memory_out, x_re, text_out
+
+def train_clip(image, text_features, y):
+    # 升维到对应空间
+    image_features = image
+
+    logits = (text_features @ image_features.T)
+
+
+    # 目标标签
+    unique_y, inverse_indices = torch.unique(y, return_inverse=True)
+    labels_cilp = torch.zeros(logits.shape)
+    for y_label in unique_y:
+        mask = (y == y_label)
+        target_y = torch.zeros(y.shape)
+        target_y[mask] = 1
+        labels_cilp[y == y_label] = target_y
+
+    logits = torch.softmax(logits, dim=-1)
+    target1 = torch.softmax(labels_cilp, dim=-1)
+
+    return criteria1(logits, target1), logits, target1
+
+
+class My_UW_Loss(nn.Module):
+    """Uncertainty Weighting
+       Kendall, A., Gal, Y., & Cipolla, R. (2018). Multi-task learning using uncertainty to weigh losses
+       for scene geometry and semantics. In Proceedings of the IEEE conference on computer vision and
+       pattern recognition(pp. 7482-7491).
+       """
+
+    def __init__(self):
+        super(My_UW_Loss, self).__init__()
+        self.a = nn.Parameter(torch.randn(1, requires_grad=True))  # 可训练参数A
+        self.b = nn.Parameter(torch.randn(1, requires_grad=True))  # 可训练参数A
+        self.log_vars = [self.a, self.b]
+
+    def forward(self, losses):
+        for i, log_var in enumerate(self.log_vars):
+            losses[i] = (1 / 2) * (torch.exp(-log_var[0]) ** 2) * losses[i] + torch.log(torch.exp(log_var[0]) + 1)
+        return sum(losses)
+
+
+if __name__ == '__main__':
+    percent = 100  # 20, 40, 60, 80
+    # 随机种子
+    # 固定随机种子等操作 30, 32
+    seed_n = 30
+    print('seed is ' + str(seed_n))
+    g = torch.Generator()
+    g.manual_seed(seed_n)
+    random.seed(seed_n)
+    np.random.seed(seed_n)
+    torch.manual_seed(seed_n)
+    torch.cuda.manual_seed(seed_n)
+    torch.cuda.manual_seed_all(seed_n)
+    os.environ['PYTHONHASHSEED'] = str(seed_n)  # 为了禁止hash随机化，使得实验可复现。
+
+    # 输入数据
+    loaded_data = data_used(percent)
+    # 超参数
+    input_dim = 13
+    num_epochs = 90
+    batch_size = 64
+    zero_dim = torch.zeros(batch_size, 1, input_dim) + 2
+    learning_rate = 3e-4
+
+    # 模型
+    model = STtrans_C_FNN()
+    # 开启dropout
+    model.train()
+    loss_function = My_UW_Loss()
+    # 优化器
+    optimizer_encoder = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer_function = torch.optim.Adam(loss_function.parameters(), lr=learning_rate)
+    # 损失函数
+    criteria = nn.MSELoss()
+    criteria1 = nn.MSELoss(reduction='sum')
+    # 训练
+    for epoch in range(num_epochs):
+        total_loss = 0
+        total_loss1, total_loss2 = 0, 0
+
+        for batch in loaded_data:
+            inputs, y_label, targets, know_inputs = batch
+            inputs = inputs.to(torch.float32)
+            targets = targets.to(torch.float32)
+            know_inputs = know_inputs.to(torch.float32)
+
+            m_out, outs, re_o = model(inputs, know_inputs)
+            # 清空梯度
+            optimizer_encoder.zero_grad()
+            optimizer_function.zero_grad()
+
+            # CLIP损失
+            loss_clip, a, b = train_clip(m_out, re_o, y_label)
+            # 重构损失
+            loss_re = criteria(outs.contiguous().view(-1), targets.contiguous().view(-1))
+            loss = loss_function([loss_clip, loss_re])
+            # 计算梯度
+            loss.backward()
+
+            # 更新参数
+            optimizer_encoder.step()
+            optimizer_function.step()
+
+            total_loss += loss.item()
+            total_loss1 += loss_re.item()
+            total_loss2 += loss_clip.item()
+        print('总损失', total_loss)
+        print('重构损失', total_loss1)
+        print('匹配损失', total_loss2)
+
+        # if (epoch + 1) % 2 == 0:
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {total_loss}')
+        # 保存模型参数
+        save_path = f'Model_parameters/model_{percent}%.pth'
+        torch.save(model.state_dict(), save_path)
+
+
---
+++ step2test_plot.py
@@ -0,0 +1,146 @@
+import numpy as np
+from step1train_our_model import STtrans_C_FNN
+import torch
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from scipy.stats import gaussian_kde
+from matplotlib.colors import ListedColormap
+from scipy.stats import wasserstein_distance
+
+
+def model_test(x, per):
+    """
+    :param x: 输入数据
+    :return: 类别 + 残差
+    """
+    # 创建一个新的模型实例，用于加载参数
+    net = STtrans_C_FNN()  # 请替换成你的模型定义
+    # 加载已保存的模型参数
+    save_path = f'Model_parameters/model_{per}%.pth'
+    net.load_state_dict(torch.load(save_path))
+    net.eval()
+    with torch.no_grad():
+        _, out, _ = net(x)
+
+        x_re = out.view(x.shape)
+    return x_re
+
+
+def load_online_data():
+    file_path_ab = 'test_dataset/test_abnormal.pt'
+    data_ab = torch.load(file_path_ab)
+    file_path = 'test_dataset/test_normal.pt'
+    data_n = torch.load(file_path)
+
+    # 精选的数据
+    idd = np.load('test_dataset/index.npy')
+    data_n = data_n[idd]
+    data_ab = data_ab[:700]
+    return data_n, data_ab
+
+
+def t_sne_plot(x_data, ab_data):
+    """
+    使用 t-SNE 降维并绘制正常数据和异常数据的散点图
+    :param x_data: 原始数据，形状为 (n_samples, n_features)
+    :param ab_data: 重构数据，形状为 (n_samples, n_features)
+    :return: 绘制t-SNE图
+    """
+    x_data = x_data.reshape(len(x_data), -1)
+    ab_data = ab_data.reshape(len(ab_data), -1)
+
+    # 合并数据集
+    combined_data = np.vstack([x_data, ab_data])
+
+    # 使用t-SNE进行降维，目标维度为2，便于绘制
+    tsne = TSNE(n_components=2, random_state=23)
+    reduced_data = tsne.fit_transform(combined_data)
+
+    # 分离降维后的数据
+    normal_data_2d = reduced_data[:len(x_data)]
+    anomaly_data_2d = reduced_data[len(x_data):]
+
+    # 为了绘制等高线图，使用核密度估计来平滑数据
+    kde_normal = gaussian_kde(normal_data_2d.T)
+    kde_anomaly = gaussian_kde(anomaly_data_2d.T)
+
+    # 创建网格
+    x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+    y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+    x_grid, y_grid = np.mgrid[x_min:x_max:100j, y_min:y_max:100j]
+
+    # 计算每个网格点的概率密度
+    grid_points = np.vstack([x_grid.ravel(), y_grid.ravel()])
+    normal_density = kde_normal(grid_points).reshape(x_grid.shape)
+    anomaly_density = kde_anomaly(grid_points).reshape(x_grid.shape)
+
+    # 绘制等高线图
+    plt.figure(figsize=(8, 6))
+    # 自定义颜色：为正常数据使用蓝色
+    normal_cmap = ListedColormap([55 / 255, 103 / 255, 149 / 255])  # 蓝色
+
+    # 自定义颜色：为异常数据使用红色
+    anomaly_cmap = ListedColormap([239 / 255, 138 / 255, 71 / 255])  # 红色
+    # 绘制正常数据的等高线图
+    plt.contour(x_grid, y_grid, normal_density, levels=10, cmap=normal_cmap, alpha=0.5)
+
+    # 绘制异常数据的等高线图
+    plt.contour(x_grid, y_grid, anomaly_density, levels=10, cmap=anomaly_cmap, alpha=0.5)
+
+    # 绘制散点图（可选，标记正常数据和异常数据）
+    plt.scatter(normal_data_2d[:, 0], normal_data_2d[:, 1], color=[55 / 255, 103 / 255, 149 / 255], label='Raw data',
+                s=20)
+    plt.scatter(anomaly_data_2d[:, 0], anomaly_data_2d[:, 1], color=[239 / 255, 138 / 255, 71 / 255],
+                label='Reconstructing data', s=20)
+
+    # 添加标题和标签
+    plt.xlabel('Component 1')
+    plt.ylabel('Component 2')
+    plt.legend()
+    plt.grid(False)
+    # 自动调整布局
+    plt.tight_layout()
+    plt.savefig(r'C:\Users\admin\Desktop\t_sne.jpg', format='jpg', dpi=800)
+    plt.show()
+
+    return normal_data_2d, anomaly_data_2d
+
+
+def reshape_data(data):
+    data_filtered = data.detach().numpy()
+    return data_filtered.reshape(len(data_filtered), -1)
+
+
+def distance_metrics(xx, yy):
+    # 计算Wasserstein距离
+    distance_d1 = wasserstein_distance(xx[:, 0], yy[:, 0])
+    distance_d2 = wasserstein_distance(xx[:, 1], yy[:, 1])
+
+    print("第一维Wasserstein Distance:", distance_d1)
+    print("第二维Wasserstein Distance:", distance_d2)
+    return (distance_d1 + distance_d2) / 2
+
+
+if __name__ == '__main__':
+    percent = 100        # 20, 40, 60, 80, 100
+    raw_normal_data, raw_abnormal_data = load_online_data()
+    normal_data = model_test(raw_normal_data, percent)
+    abnormal_data = model_test(raw_abnormal_data, percent)
+
+    # 正常数据
+    raw_normal_data = reshape_data(raw_normal_data)
+    normal_data = reshape_data(normal_data)
+
+    # 异常数据
+    raw_abnormal_data = reshape_data(raw_abnormal_data)
+    abnormal_data = reshape_data(abnormal_data)
+
+    # 绘图t-sne
+    x, x_r = t_sne_plot(raw_normal_data, normal_data)
+    x1 = distance_metrics(x, x_r)
+
+    x, x_r = t_sne_plot(raw_abnormal_data, abnormal_data)
+    x2 = distance_metrics(x, x_r)
+
+    print('异常灵敏度:', np.sqrt(np.tanh(x2) * (np.tanh(-x1) + 1)))
+
